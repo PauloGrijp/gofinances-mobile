c25f48abcaa33b5372c99be7a55debfc26ed2adf
Paulo Xavier <pxavierf@gmail.com>
2021-12-10 12:27:05 -0300

    salvando no AsyncStorage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -3,6 +3,9 @@ import { Alert, Keyboard, Modal, TouchableWithoutFeedback } from 'react-native';
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import  AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+import { useNavigation, NavigationProp, ParamListBase } from '@react-navigation/native';
 
 import { Button } from '../../components/Form/Button';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
@@ -34,10 +37,14 @@ export function Register() {
   })
   const [transactionType, setTransactionType] = useState('');
   const [categoryModal, setCategoryModal] = useState(false);
+  const dataKey = '@gofinances:transaction';
+
+  const { navigate }: NavigationProp<ParamListBase> = useNavigation();
 
   const {
     control,
     handleSubmit,
+    reset,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
@@ -47,21 +54,45 @@ export function Register() {
     setTransactionType(type);
   }
 
-  function handleRegister(form: FormData) {
+  async function handleRegister(form: FormData) {
     if (!transactionType) return Alert.alert('Selecione o tipo de transação');
     if (category.key === 'category') return Alert.alert('Selecione o tipo de categoria');
 
-
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
       category: category.key,
+      data: new Date(),
     }
-    console.log(data)
+
+    try {
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [
+        ...currentData,
+        newTransaction,
+      ]
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+      reset();
+      setTransactionType('')
+      setCategory({
+        key: 'category',
+        name: 'Categoria',
+      });
+
+      navigate('Listagem');
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possível salvar');
+    }
   }
 
-  console.log(categoryModal)
   function handleCloseSelectCategory() {
     setCategoryModal(false);
   }
