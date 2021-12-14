ab170eb95a8341127b6f783d2e24fba24bcf5ff1
Paulo Xavier <pxavierf@gmail.com>
2021-12-14 14:06:44 -0300

    criando tela de login

--- App.tsx
+++ App.tsx
@@ -18,6 +18,8 @@ import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native'
 import { AppRoutes } from './src/routes/app.routes';
 
+import SignIn from './src/screens/SignIn';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -33,7 +35,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
       <StatusBar translucent backgroundColor={theme.colors.primary}/>
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
