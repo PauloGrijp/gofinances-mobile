a7ecfaa57e527182d311558fee7677bf8b854ae0
Paulo Xavier <pxavierf@gmail.com>
2021-12-14 16:47:13 -0300

    autorizaçao

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { AppRoutes } from './src/routes/app.routes';
 
 import SignIn from './src/screens/SignIn';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -34,8 +35,10 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-      <StatusBar translucent backgroundColor={theme.colors.primary}/>
-        <SignIn />
+        <StatusBar translucent backgroundColor={theme.colors.primary}/>
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
